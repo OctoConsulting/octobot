c526f59264f85e6f2f804e5bcc3a98f2a1f4f2e2
David Zhao <davidzhao058@gmail.com>
2018-01-22 20:11:16 -0500

    Add general TODOs

---
+++ lambda/lex_responder.py
@@ -0,0 +1,8 @@
+def lambda_handler(event, context):
+    # event.currentIntent.name + event.bot.version
+    
+    # TODO: look up intent response in dynamodb
+    
+    # TODO: assemble return package to lex bot
+    
+    return 'Hello from Lambda'
