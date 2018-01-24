afaa5ac9dedb5bc6ae09798872ad4c684e8bdeb9
David Zhao <davidzhao058@gmail.com>
2018-01-23 23:13:55 -0500

    Look up bot and intent and return response to Lex

--- lambda/lex_responder.py
+++ lambda/lex_responder.py
@@ -1,8 +1,35 @@
+import boto3
+
 def lambda_handler(event, context):
-    # event.currentIntent.name + event.bot.version
+    bot_name = event['bot']['name']
+    current_intent_name = event['currentIntent']['name']
     
-    # TODO: look up intent response in dynamodb
+    table_name = bot_name + '_intents'
+    ddb_client = boto3.client('dynamodb')
     
-    # TODO: assemble return package to lex bot
+    # TODO: add error handling
+    get_item_response = ddb_client.get_item(
+        TableName=table_name,
+        Key={
+            'intent': {
+                'S': current_intent_name
+            },
+            'version': {
+                'S': '$LATEST'
+            }
+        }
+    )
+    response_text = str(get_item_response['Item']['response']['S'])
     
-    return 'Hello from Lambda'
+    return_package = {
+        'dialogAction': {
+            'type': 'Close',
+            'fulfillmentState': 'Fulfilled',
+            'message': {
+                'contentType': 'PlainText',
+                'content': response_text
+            }
+        }
+    }
+    
+    return return_package
