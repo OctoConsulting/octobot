450fd9b8f03af8799cbfed29f5dc96673e0d090e
David Zhao <davidzhao058@gmail.com>
2018-01-23 23:21:10 -0500

    Assign LexResponder as fulfillment activity codehook

--- lambda/create_lex_bot.py
+++ lambda/create_lex_bot.py
@@ -6,28 +6,32 @@ def lambda_handler(event, context):
     # TODO: split code off into functions as necessary
     
     # Add intents
-    # assert 'intents' in event and len(event['intents']) > 0
     intent_names = []
     create_intent_responses = []
     for intent in event['intents']:
+        # TODO: add exception handling to handle if intent already exists
         intent_names.append(event['bot_name'] + '_' + intent['name'])
         client.put_intent(
             name=intent_names[-1],
             sampleUtterances=intent['sample_utterances'],
+            # TODO: change fulfillment activity to use LexResponder lambda function
             fulfillmentActivity={
-                'type': 'ReturnIntent'
-            }
+                'type': 'CodeHook',
+                'codeHook': {
+                    'uri': 'arn:aws:lambda:us-east-1:749091557667:function:LexResponder',
+                    'messageVersion': '$LATEST'
+                }
+            },
         )
         
+    # TODO: add exception handling to handle if bot already exists
     # Create bot
-    assert 'bot_name' in event
     intent_objects = []
     for intent_name in intent_names:
         intent_objects.append({
             'intentName': intent_name,
             'intentVersion': '$LATEST'
         })
-    print(event['bot_name'])
     create_bot_response = client.put_bot(
         name=event['bot_name'],
         intents=intent_objects,
@@ -61,4 +65,3 @@ def lambda_handler(event, context):
     for create_intent_response in create_intent_responses:
         response_string += str(create_intent_response) + '\n\n'
     return response_string
-        
