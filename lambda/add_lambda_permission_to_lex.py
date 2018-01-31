dfb76d047823668e0ef675e2f6227539fce29471
David Zhao <davidzhao058@gmail.com>
2018-01-31 18:23:22 -0500

    Add lambda function to give appropriate permissions to lex

---
+++ lambda/add_lambda_permission_to_lex.py
@@ -0,0 +1,14 @@
+import boto3
+
+lambda_client = boto3.client('lambda')
+
+def lambda_handler(event, context):
+    response = lambda_client.add_permission(
+        Action='lambda:InvokeFunction',
+        FunctionName='LexResponder',
+        Principal='lex.amazonaws.com',
+        # SourceAccount='123456789012',
+        SourceArn='arn:aws:lex:us-east-1:749091557667:intent:*',
+        StatementId='ID-3',
+    )
+    return 'Hello from Lambda'
