1b7fc3e1e3ff661568ad08ada86e4837e4ed19b5
David Zhao <davidzhao058@gmail.com>
2018-01-31 20:31:03 -0500

    Split code into functions

--- webapp/js/index.js
+++ webapp/js/index.js
@@ -5,65 +5,84 @@ const enableChat = () => {
 	$('#chat-log-list li').css('border-bottom', 'solid 1px #e0e0e0');
 }
 
-$(document).ready(function () {
-	AWS.config.update({ region: 'us-east-1' });
-	AWS.config.credentials = new AWS.CognitoIdentityCredentials({ IdentityPoolId: 'us-east-1:1252432e-9cd3-479d-98e5-ea5a26878766' });
+const log = (message) => {
+	const logContainer = $('#logs');
+	logContainer.html(logContainer.html() + '<br>' + message);
+}
 
-	var lambda = new AWS.Lambda({ region: 'us-east-1', apiVersion: '2015-03-31' });
-	var pullParams = {
+const configureAWS = () => {
+	AWS.config.update({ region: 'us-east-1' });
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({ IdentityPoolId: 'us-east-1:1252432e-9cd3-479d-98e5-ea5a26878766' });
+}
+
+const createBot = (url) => {
+	const lambda = new AWS.Lambda({ region: 'us-east-1', apiVersion: '2015-03-31' });
+	const pullParams = {
 		FunctionName: 'CreateAzureKnowledgeBase',
 		InvocationType: 'RequestResponse',
 		LogType: 'None',
+		Payload: JSON.stringify({'url': url})
 	};
 
-	var lex = new AWS.LexRuntime({ apiVersion: '2016-11-28' });
+	lambda.invoke(pullParams, function (err, data) {
+		if (err) {
+			log(err);
+		} else {
+			pullResults = JSON.parse(data.Payload);
+			console.log(pullResults);
+			if (pullResults['succeeded']) {
+				botName = pullResults['bot_name'].trim();
+				log(data.Payload);
+				$('#url-input').data('botName', botName);
+				enableChat();
+			} else {
+				log(data.Payload);
+			}
+		}
+	});
+}
 
-	let botName = '';//'Ramhacksvcuedu';
+const appendMessageToChat = (user, message) => {
+	$('#chat-log-list').append('<li>' + user + ': ' + message + '</li>');
+}
+
+
+const messageLex = (botName, message) => {
+	let lex = new AWS.LexRuntime({ apiVersion: '2016-11-28' });
+	const params = {
+		botAlias: 'DEV',
+		botName: botName,
+		inputText: message,
+		userId: 'demo-webapp-id',
+	};
+	lex.postText(params, function (err, data) {
+		if (err) {
+			console.log(err, err.stack);
+		} else {
+			console.log(data);
+			appendMessageToChat('Bot', data['message']);
+		}
+	});
+}
+
+$(document).ready(function () {
+	configureAWS();
 
 	$('#create-bot').submit(function (e) {
 		e.preventDefault();
 		$(this).children('input').prop('disabled', true);
 		url = $(this).children('#url-input').val();
-		pullParams['Payload'] = JSON.stringify({
-			'url': url
-		});
-		lambda.invoke(pullParams, function (err, data) {
-			if (err) {
-				$('#logs').html($('#logs').html() + err);
-			} else {
-				pullResults = JSON.parse(data.Payload);
-				console.log(pullResults)
-				if(pullResults['succeeded']) {
-					botName = pullResults['bot_name'].trim();
-					$('#logs').html($('#logs').html() + '<br>' + data.Payload);
-					$('#url-input').data('botName', botName);
-					enableChat();
-				} else {
-					$('#logs').html($('#logs').html() + data.Payload);
-				}
-			}
-		});
+		createBot(url);
 	});
 
 	$('#message-submit').click(function (e) {
 		let messageInput = $('#message-input');
 		let message = messageInput.val();
-		$('#chat-log-list').append('<li>' + 'You: ' + message + '</li>'); 
-		if(message.trim() == '') return;
-		messageInput.val('');
-		var params = {
-			botAlias: 'DEV', /* required */
-			botName: $('#url-input').data('botName'), /* required */
-			inputText: message, /* required */
-			userId: 'demo-webapp-id', /* required */
-		};
-		lex.postText(params, function (err, data) {
-			if (err) {
-				console.log(err, err.stack); // an error occurred
-			} else {
-				console.log(data);
-				$('#chat-log-list').append('<li>' + 'Bot: ' + data['message'] + '</li>');           // successful response
-			}
-		});
+		let botName = $('#url-input').data('botName');
+		appendMessageToChat('You', message);
+		if (message.trim() != '' && botName !== undefined) {
+			messageInput.val('');
+			messageLex(botName, message);
+		}
 	})
 });
